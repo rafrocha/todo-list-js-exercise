eec8b23b493e6c4c332c4017d693c9ff8db16b3d
Rafael <rafaelmarianorocha@gmail.com>
2018-08-01 07:50:21 +0000

    Adds comment

--- index.js
+++ index.js
@@ -15,12 +15,10 @@ function newTask(title, description) {
   return task;
 }
 
-// DRIVER CODE CHANGES BELOW
-
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
 
 task1.logState(); // Clean Cat Litter has not been completed
-task1.markCompleted();
+task1.markCompleted(); //Makes it completed (true);
 task1.logState(); // Clean Cat Litter has been completed
